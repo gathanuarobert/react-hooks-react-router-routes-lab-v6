bf08a0326b92a1d7c81cbecea193f9d3a84d6056
Robert Gathanua <robertgathanua@gamail.com>
2024-05-11 09:13:04 +0300

    complete lab

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,16 +1,30 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
-function Actors() {
+function Actors({actors}) {
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
         {/* Actor info here! */}
+        <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <article key={index}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </article>
+         ))}
       </main>
     </>
   );
 };
 
 export default Actors;
+
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,41 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import { useParams } from 'react-router-dom';
 
 function Movie() {
+  const [movie, setMovie] = useState({});
+  const params = useParams();
+  const movieId = params.id;
+
+  useEffect(() =>{
+    fetch(`http://your-api-url/movies/${movieId}`)
+    .then(r => r.json())
+    .then(data => setMovie(data))
+    .catch(error => console.error(error));
+  }, [movieId]);
+
+  if(!movie){
+    return <h1>Loading...</h1>;
+  };
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
         {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+      <p>Time: {movie.time}</p>
+      <div>
+        Genres: {movie.genres.map((genre, index) => (
+          <span key={index}>{genre}</span>
+        ))}
+      </div>  
       </main>
     </>
   );
 };
 
 export default Movie;
+
